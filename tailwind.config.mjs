d12bf31829cd79858893a37a2fd6d4284ff68d5b
Anabella <rodriguezanabella094@gmail.com>
2024-07-25 15:24:06 -0300

    Format

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,14 +4,14 @@ export default {
   theme: {
     extend: {
       colors: {
-        "primary-color":   "#5eb9f0",
+        "primary-color": "#5eb9f0",
         "paragraph-color": "#60697b",
-        "heading-color":   "#343f52",
-         skyblue:          "#e5f4fd",
-         white:            "#ffffff",
-        "light-gray":      "#f0f8fe",
-         gray:             "#fefefe",
-        "footer-p":        "#CACACA",
+        "heading-color": "#343f52",
+        skyblue: "#e5f4fd",
+        white: "#ffffff",
+        "light-gray": "#f0f8fe",
+        gray: "#fefefe",
+        "footer-p": "#CACACA",
       },
     },
   },
